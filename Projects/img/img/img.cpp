32486c307f90a8ce326c8177c783307278c25339
Unknown <lqwn16@student.aau.dk>
2017-10-20 13:42:19 +0200

    update2

--- Projects/img/img/img.cpp
+++ Projects/img/img/img.cpp
@@ -4,6 +4,7 @@
 
 #include "stdafx.h"
 #include "opencv2/core/core.hpp"
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
 
@@ -11,15 +12,22 @@ using namespace std;
 using namespace cv;
 
 void loadpicture();
+void histogram();
+void histcounter();
 void equa();
 
-
+int histo[];
+int histo2[];
+int histogramcount[];
 Mat img;
+
 
 
 int main()
 {
 	loadpicture();
+	histogram();
+	histcounter();
 	equa();
 
 	return 0;
@@ -27,9 +35,34 @@ int main()
 
 void loadpicture() {
 	img = imread("MyPic.jpg");
-	namedWindow("image", CV_WINDOW_AUTOSIZE);
-	imshow("image", img);
+	namedWindow("Input image", CV_WINDOW_AUTOSIZE);
+	imshow("Input image", img);
 
+}
+
+void histogram(){
+
+	// initialize all intensity values to 0
+	for (int i = 0; i < 256; i++)
+	{
+		histo[i] = 0;
+	}
+
+	// calculate the no of pixels for each intensity values
+	for (int y = 0; y < img.rows; y++)
+		for (int x = 0; x < img.cols; x++)
+			histo[(int)img.at<uchar>(y, x)]++;
+
+}
+
+void histcounter()
+{
+	histogramcount[0] = histo2[0];
+
+	for (int i = 1; i < 256; i++)
+	{
+		histogramcount[i] = histo2[i] + histogramcount[i - 1];
+	}
 }
 
 void equa() {
@@ -37,14 +70,10 @@ void equa() {
 
 	Mat new_image = img.clone();
 
-	if (img.data && !img.empty()) {
-		for (size_t y = 0; y < img.rows; ++y) {
-			for (size_t x = 0; x < img.cols; ++x) {
-				
-			}
-		}
+
+		
 		namedWindow("histogram equalization", CV_WINDOW_AUTOSIZE);
 		imshow("histogram equalization", img);
 		waitKey(0);
-	}
+	
 }
